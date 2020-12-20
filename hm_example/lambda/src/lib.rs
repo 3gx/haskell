6555bc182bb7d8de57aa905e7896ee2c6865c014
Evghenii <egaburov@nvidia.com>
2020-12-20 09:24:38 -0800

    checkpoint

--- hm_example/lambda/src/lib.rs
+++ hm_example/lambda/src/lib.rs
@@ -1,7 +1,7 @@
-
 type Int = i32;
 type DynTerm = Box<Term>;
-enum Term {
+#[derive(PartialEq, Debug)]
+pub enum Term {
     Lam(String, DynTerm),
     Var(String),
     App(DynTerm, DynTerm),
@@ -10,11 +10,32 @@ enum Term {
 }
 
 type DynTyp = Box<Typ>;
-enum Typ {
+#[derive(PartialEq, Debug, Clone)]
+pub enum Typ {
     TLam(String, DynTyp),
     TBVar(String),
     TVar(Int),
     TArr(DynTyp, DynTyp),
     TInt,
     TUnit,
+}
+
+fn mk_new<T>(val: T) -> Box<T> {
+    Box::new(val)
+}
+
+pub fn subst_ty(from: Typ, to: Typ, typ: Typ) -> Typ {
+    use Typ::{TArr, TLam};
+    if from == typ {
+        to
+    } else {
+        match typ {
+            TLam(v, ty) => TLam(v, mk_new(subst_ty(from, to, *ty.clone()))),
+            TArr(ty1, ty2) => TArr(
+                mk_new(subst_ty(from.clone(), to.clone(), *ty1)),
+                mk_new(subst_ty(from.clone(), to.clone(), *ty2)),
+            ),
+            _ => typ,
+        }
+    }
 }
