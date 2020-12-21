615837aa391730ed0b1836a8484aaa84160f5c82
Evghenii <egx@egx-mbp.local>
2020-12-21 10:33:56 -0800

    some println! examples

--- hm_example/lambda/src/main.rs
+++ hm_example/lambda/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     use lambda::*;
     println!("Hello, world!");
-    println!("term_id= {}", term_id());
-    println!("term_int= {}", term_int());
+    println!("term_id= {x}", x = term_id());
+    println!("term_int= {term_int}", term_int = term_int());
     println!("term_id_unit= {}", term_id_unit());
     println!("term_higher= {}", term_higher());
     println!("term_occurs= {}", term_occurs());
